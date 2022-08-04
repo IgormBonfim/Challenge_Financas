31d3a60b500e8c1fb0e61efb7bca6b6ad9114329
Igor Bonfim <igormbonfim@hotmail.com>
2022-08-03 23:30:23 -0300

    Adicionado IUsuario IDespesa

---
+++ Dominio/Servicos/ServicoReceita.cs
@@ -0,0 +1,36 @@
+﻿using Dominio.Interfaces;
+using Dominio.Interfaces.InterfaceServicos;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Servicos
+{
+    public class ServicoReceita : IServicoReceita
+    {
+        private readonly IReceita iReceita;
+
+        public ServicoReceita(IReceita iReceita)
+        {
+            this.iReceita = iReceita;
+        }
+
+        public async Task AdicionaReceita(Receita receita)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task AtualizaReceita(Receita receita)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Receita>> ListarReceitas()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
